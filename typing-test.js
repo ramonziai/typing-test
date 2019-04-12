d1ded48a7f011910eb1613d1e07bb85c58c557b1
Ramon Ziai <rziai@sfs.uni-tuebingen.de>
2019-04-12 12:37:16 +0200

    Fix Firefox problem where nothing was displayed, increase font size in footer

--- typing-test.js
+++ typing-test.js
@@ -4,9 +4,6 @@ window.$ = document.querySelectorAll.bind(document);
 // Changes for  Firefox
 if (navigator.userAgent.match(/firefox/i)) {
     // Unicode font sizes
-    let ffBtn = "font-weight: normal; font-size: 2em; margin-left: 0.3em;";
-    $("#restart-symbol")[0].setAttribute("style", ffBtn);
-
     let ffwait = "line-height: 1em; font-size: 4em;";
     $(".waiting")[0].setAttribute("style", ffwait);
 }
